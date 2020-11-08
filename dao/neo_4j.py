08800c8c1d95142991f632921491a29ee6d1916a
chu <3339607643@qq.com>
2020-11-08 21:52:27 +0800

    ly+2020/11/8

--- dao/neo_4j.py
+++ dao/neo_4j.py
@@ -1,10 +1,11 @@
 from py2neo import Graph, Node, Relationship
+import re,requests
 
 graph = Graph("http://localhost:7474", username="neo4j", password="Xc200215")
 
 # 小说 作者 名字 链接
 def get_novel_info():
-    match_str = 'MATCH (p:novel) return p.author as author,p.name as name,p.href as href,p.imgurl as imgurl limit 5'
+    match_str = 'MATCH (p:novel) return p.author as author,p.name as name,p.href as href,p.imgurl as imgurl limit 8'
     items = graph.run(match_str).data()
     info = get_detail(items)
     return info
@@ -15,6 +16,15 @@ def get_novel_content(name, author):
     match_str = 'MATCH (n:novel {name:"' + name + '",author:"' + author + '"})-[r:novel_chapter]->(c:chapter) RETURN c.chapterurl as chapterurl,c.name as chapterName'
     items = graph.run(match_str).data()
     content = get_detail(items)
+    return content
+
+#小说 详情
+def get_novel_detail(name,author):
+    match_str='match (n:novel {name:"'+name+'",author:"'+author+'"}) return n.name as name,n.author as author,n.href as href,n.imgurl as imgurl,n.type as type,n.intrduce as intro'
+    items=graph.run(match_str).data()
+    content = get_detail(items)
+    for item in content:
+        print(item)
     return content
 
 # 通过小说名字或者作者查询小说
@@ -29,14 +39,11 @@ def get_detail(items):
     novel_info = []
     for item in items:
         novel_info.append(item)
-    # print(novel_info)
     return novel_info
 
 def felei(type):
-    match_str = 'MATCH (:type {name:"'+type+'"})-[:novel_type]->(p:novel) RETURN p.author as author,p.name as name,p.href as href,p.imgurl as imgurl limit 5'
+    match_str = 'MATCH (:type {name:"'+type+'"})-[:novel_type]->(p:novel) RETURN p.author as author,p.name as name,p.href as href,p.imgurl as imgurl limit 9'
     items = graph.run(match_str).data()
     info = get_detail(items)
     return info
 
-# x=get_novel("斗罗大陆")
-# print(x)
